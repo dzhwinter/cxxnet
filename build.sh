5c7803c7a0fe11a39d3fc7edca079e1cf85cd114
dzhwinter <dzhwinter@gmail.com>
2015-07-28 15:17:12 +0800

    "use git submodule manage project"

--- build.sh
+++ build.sh
@@ -1,17 +1,5 @@
 #! /bin/bash
 
-if [ ! -d mshadow ]; then
-    git clone https://github.com/dmlc/mshadow.git
-fi
-
-if [ ! -d rabit ]; then
-    git clone https://github.com/dmlc/rabit.git
-fi
-
-if [ ! -d dmlc-core ]; then
-    git clone https://github.com/dmlc/dmlc-core.git
-fi
-
 
 if [ ! -f config.mk ]; then
     echo "Use the default config.m"
--- build_ps.sh
+++ build_ps.sh
@@ -1,23 +1,10 @@
 #! /bin/bash
 
 if [ ! -d ps-lite ]; then
-    git clone https://github.com/dmlc/ps-lite
     echo "Install third-party libraries for PS"
     ./ps-lite/make/install_deps.sh 1>install_ps.log
     echo "Compile PS"
     make -j 4 -C ps-lite
-fi
-
-if [ ! -d rabit ]; then
-    git clone https://github.com/dmlc/rabit.git
-fi
-
-if [ ! -d mshadow ]; then
-    git clone https://github.com/dmlc/mshadow.git
-fi
-
-if [ ! -d dmlc-core ]; then
-    git clone https://github.com/dmlc/dmlc-core.git
 fi
 
 if [ ! -f config.mk ]; then
